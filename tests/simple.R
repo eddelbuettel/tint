dcabbaa317553e1bc151515f35a8ca437351570a
Dirk Eddelbuettel <edd@debian.org>
2016-10-15 14:41:02 -0500

    release 0.0.3
    several tweaks
    added NEWS file
    coloured links in pdf
    added (conditional) pdf test

--- tests/simple.R
+++ tests/simple.R
@@ -1,7 +1,16 @@
 
-setwd(tempdir())
+## run in the R session temp.dir. which gets cleaned
+setwd(tempdir())                        
+
+## process html -- should work everywhere
 file.copy(system.file("examples", "simpleHtml.Rmd", package="tint"), "simpleHtml.Rmd")
 rmarkdown::render("simpleHtml.Rmd")
+
+## pdf requires a fairly complete latex installation so make this conditional
+if (isTRUE(unname(Sys.info()["user"])=="edd")) {
+    file.copy(system.file("examples", "simplePdf.Rmd", package="tint"), "simplePdf.Rmd")
+    rmarkdown::render("simplePdf.Rmd")
+}
 
 
 
