6972729481463f30fff13788bea6301a23d2c70f
Dirk Eddelbuettel <edd@debian.org>
2020-07-13 13:38:20 -0500

    update parse_footnotes for html mode, roll minor version

--- R/html.R
+++ R/html.R
@@ -192,15 +192,19 @@ tint_html_dependency = function() {
 
 # we assume one footnote only contains one paragraph here, although it is
 # possible to write multiple paragraphs in a footnote with Pandoc's Markdown
-parse_footnotes = function(x) {
+parse_footnotes = function(x, fn_label='fn') {
   i = which(x == '<div class="footnotes">')
   if (length(i) == 0) return(list(items = character(), range = integer()))
   j = which(x == '</div>')
   j = min(j[j > i])
   n = length(x)
-  r = '<li id="fn([0-9]+)"><p>(.+)<a href="#fnref\\1">.</a></p></li>'
+  r = sprintf(
+    '<li id="%s([0-9]+)"><p>(.+)<a href="#%sref\\1"([^>]*)>.{1,2}</a></p></li>',
+    fn_label, fn_label
+  )
+  s = paste(x[i:j], collapse = '\n')
   list(
-    items = gsub(r, '\\2', grep(r, x[i:n], value = TRUE)),
+    items = gsub(r, '\\2', unlist(regmatches(s, gregexpr(r, s)))),
     range = i:j
   )
 }
